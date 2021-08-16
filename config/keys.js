5ae2dcbfc09f7f13e640567723f0799d8c5fb7c7
najibshah <najeebworkmail@gmail.com>
2021-08-16 22:30:53 +0500

    initial commit

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 // (2nd file created)
 module.exports = {
   mongoURI:
-    "mongodb+srv://najib:ifkq5tcI7Gr75AjC@cluster0.d1jrj.mongodb.net/FilmabadBetaDatabase?retryWrites=true&w=majority&ssl=true",
+    "mongodb+srv://najib:ifkq5tcI7Gr75AjC@cluster0.d1jrj.mongodb.net/FilmabadBetaDatabase?retryWrites=true&w=majority",
   // "mongodb://najib:najib@projectcluster01-shard-00-00-2rois.mongodb.net:27017,projectcluster01-shard-00-01-2rois.mongodb.net:27017,projectcluster01-shard-00-02-2rois.mongodb.net:27017/test?ssl=true&replicaSet=ProjectCluster01-shard-0&authSource=admin&retryWrites=true",
   secretOrKey: "secret",
 };
